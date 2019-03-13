63578fa187fdd8c6b1507ce2e010bf952b647a6e
Bono IJpelaar <ijpelaar_9@hotmail.com>
2019-03-13 11:53:28 +0100

    Generated car-management component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { CarManagementComponent } from './components/car-management/car-management.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    CarManagementComponent
   ],
   imports: [
     BrowserModule,
