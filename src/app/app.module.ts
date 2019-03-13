9a3fb4d3c536012338c8db04b22aa618da7e2ed3
reinoudvanzoelen <reinoud95@hotmail.com>
2019-03-13 11:37:32 +0100

    first layout of the header, split into logobar and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Components
+import { HeaderComponent } from './components/header/header.component';
+import { HeaderLogobarComponent } from './components/header-logobar/header-logobar.component';
+import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
+
+// Material Angular Components
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // App Components
+    HeaderComponent,
+    HeaderLogobarComponent,
+    HeaderNavbarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // Material Angular Components
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
