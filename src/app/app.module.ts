25dd75780ad660b1ccfc9ed104ec333f6bce5f82
reinoudvanzoelen <reinoud95@hotmail.com>
2019-03-14 16:05:46 +0100

    added mocking for Cars Owners and Trackers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './components/landing/landing.component';
 
 // Components
 import { HeaderComponent } from './components/header/header.component';
 import { HeaderLogobarComponent } from './components/header-logobar/header-logobar.component';
 import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 // Material Angular Components
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// API 
+import { HttpClientModule } from '@angular/common/http';
+
+// MOCK Api
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService as InMemoryDataService } from './services/api/InMemory/inmemory.data.service'
 
 @NgModule({
   declarations: [
@@ -25,10 +32,13 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    // API Mocking
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
     // Material Angular Components
     MatToolbarModule
   ],
-  providers: [],
+  providers: [InMemoryDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { CarService } from 'src/app/services/car/car.service';
+import { OwnerService } from 'src/app/services/owner/owner.service';
+import { TrackerService } from 'src/app/services/tracker/tracker.service';
 
 @Component({
   selector: 'rr-landing',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient,
+    private carService: CarService,
+    private ownerService: OwnerService,
+    private trackerService: TrackerService) { }
 
   ngOnInit() {
-  }
+    this.carService.getAll().subscribe(x => {
+      console.log(x);
+    });
 
+    this.ownerService.getAll().subscribe(x => {
+      console.log(x);
+    });
+
+    this.trackerService.getAll().subscribe(t => {
+      console.log(t);
+    });
+  }
 }
