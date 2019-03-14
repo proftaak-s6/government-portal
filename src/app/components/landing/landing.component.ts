24e70155d9c5426ab313e496ebc86304a4e970c6
reinoudvanzoelen <reinoud95@hotmail.com>
2019-03-14 16:57:55 +0100

    cleaned up the landing component

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CarService } from 'src/app/services/car/car.service';
-import { OwnerService } from 'src/app/services/owner/owner.service';
-import { TrackerService } from 'src/app/services/tracker/tracker.service';
 
 @Component({
   selector: 'rr-landing',
@@ -11,22 +7,8 @@ import { TrackerService } from 'src/app/services/tracker/tracker.service';
 })
 export class LandingComponent implements OnInit {
 
-  constructor(private http: HttpClient,
-    private carService: CarService,
-    private ownerService: OwnerService,
-    private trackerService: TrackerService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.carService.getAll().subscribe(x => {
-      console.log(x);
-    });
-
-    this.ownerService.getAll().subscribe(x => {
-      console.log(x);
-    });
-
-    this.trackerService.getAll().subscribe(t => {
-      console.log(t);
-    });
   }
 }
