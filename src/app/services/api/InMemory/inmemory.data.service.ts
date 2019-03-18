80529530f2f4451d60e8f826996065987c63fb93
reinoudvanzoelen <reinoud95@hotmail.com>
2019-03-18 13:55:19 +0100

    added a third car

--- src/app/services/api/InMemory/inmemory.data.service.ts
+++ src/app/services/api/InMemory/inmemory.data.service.ts
@@ -10,21 +10,24 @@ import { Tracker } from 'src/entities/Tracker';
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
+    const niels_Tracker: Tracker = new Tracker("Tankimax");
+    const niels_Car: Car = new Car("TD-NR-98", "Car", "Large", "Biodiesel", "B+", niels_Tracker);
+
     const bono_PersonalInfo: PersonalInfo =
       new PersonalInfo("Bono", "IJpelaar", new Date(1995, 4, 24), "Enschotsestraat", "111-24", "5014DD", "Tilburg", "The Netherlands");
     const bono_Tracker: Tracker = new Tracker("DTP");
-    const bono_Car: Car = new Car("GG-WP-69", "Car", "Medium", "Benzine", "B-", bono_Tracker);
+    const bono_Car: Car = new Car("32-LP-VV", "Car", "Medium", "Benzine", "B-", bono_Tracker);
     const bono_Owner: Owner = new Owner(bono_PersonalInfo, new Array<Car>(bono_Car));
 
     const reinoud_PersonalInfo: PersonalInfo =
       new PersonalInfo("Reinoud", "van Zoelen", new Date(1995, 5, 22), "Hertogensingel", "31", "5341AB", "Oss", "The Netherlands");
     const reinoud_Tracker: Tracker = new Tracker("ACG");
     const reinoud_Car: Car = new Car("OMG-WTF", "Car", "Small", "Diesel", "A+", reinoud_Tracker);
-    const reinoud_Owner: Owner = new Owner(reinoud_PersonalInfo, new Array<Car>(reinoud_Car));
+    const reinoud_Owner: Owner = new Owner(reinoud_PersonalInfo, new Array<Car>());
 
-    const trackers: Tracker[] = [bono_Tracker, reinoud_Tracker]
+    const trackers: Tracker[] = [bono_Tracker, reinoud_Tracker, niels_Tracker];
+    const cars: Car[] = [bono_Car, reinoud_Car, niels_Car];
     const owners: Owner[] = [bono_Owner, reinoud_Owner];
-    const cars: Car[] = [bono_Car, reinoud_Car];
 
     return {
       cars: cars,
